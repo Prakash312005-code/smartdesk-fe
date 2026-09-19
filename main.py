c9680820e17a48e5a7a4cfccf20f567b02be1aae
Prakash312005-code <prakash.a312005@gmail.com>
2026-09-19 10:56:27 +0530

    Configure backend CORS

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
-from models.admin import Admin
+from fastapi.middleware.cors import CORSMiddleware
+
 from database.connection import engine, Base
 from models.ticket import Ticket
+from models.admin import Admin
+from models.status_history import StatusHistory
+
 from routes.ticket_route import router as ticket_router
 from routes.auth_route import router as auth_router
-from models.status_history import StatusHistory
 from routes.dashboard_route import router as dashboard_router
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
